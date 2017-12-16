da06ba310710005da204278d05b76248c717471d
AIexoaner <AIexoaner>
2017-12-16 17:56:00 +0100

    Test VerifyListProductCount()
    Adding new test VerifyListProductCount()

--- UnitTestProject.Tests/ControllerTest.cs
+++ UnitTestProject.Tests/ControllerTest.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using UnitTestProject.Web.Controllers;
+using UnitTestProject.Web.Models;
 using Xunit;
+using System.Linq;
 
 namespace UnitTestProject.Tests
 {
@@ -18,6 +21,15 @@ namespace UnitTestProject.Tests
 
             // Assert
             Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void VerifyListProductCount()
+        {
+            var controller = new HomeController();
+            var result = Assert.IsType<ViewResult>(controller.List());
+            var model = Assert.IsType<List<Product>>(result.Model);
+            Assert.Equal(2,model.Count());
         }
     }
 }
