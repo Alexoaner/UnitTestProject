901e8654f9c051350c87fb988ade456fbf9cda56
AIexoaner <AIexoaner>
2017-12-16 17:39:45 +0100

    Adding List method in Home and a View
    Adding List method in Home and a View

--- UnitTestProject.Web/Controllers/HomeController.cs
+++ UnitTestProject.Web/Controllers/HomeController.cs
@@ -1,14 +1,26 @@
-
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using UnitTestProject.Web.Models;
 
 namespace UnitTestProject.Web.Controllers
 {
     public class HomeController : Controller
     {
-        // GET
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var products = new List<Product>{
+                new Product { ID = 1, Name = "Apples", Price = 1.50m },
+                new Product { ID = 2, Name = "Bananas", Price = 2.00m }
+            };
+
+            return View(products);
         }
     }
 
